2f0857d9e0a54e980bd6037e5d4a3366cf0a276a
ViniciussMelo <vinicius.smelo54@gmail.com>
2023-05-25 16:26:04 -0300

    feat[api.consumer]: added api prefix configuration

--- api.consumer/src/main.ts
+++ api.consumer/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  dotenv.config();
+
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  app.setGlobalPrefix(process.env.API_PREFIX);
+
+  const port = process.env.PORT || 3002;
+
+  await app
+    .listen(port)
+    .then(() => console.log(`Application running at port: ${port}`));
 }
 bootstrap();
